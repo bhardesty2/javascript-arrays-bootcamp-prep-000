c025f6c4f51368708f29fc0342241492b7afd34c
Blake Hardesty <ha1003218@email.ccbcmd.edu>
2019-01-15 01:23:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,51 +1,6 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kit kat', 'skittles']
-
-function addElementToBeginningOfArray(array, element){
-  return [element, ...array];
-}
-
-function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element);
-  return array;
-}
-
-function addElementToEndOfArray(array, element){
-  return [...array, element];
-}
-
-function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element);
-  return array;
-}
-
-function removeElementFromBeginningOfArray(array){
-  return array.slice(1);
-}
-
-function removeElementFromEndOfArray(array){
-  return array.slice(0, array.length -1);
-}
-
-function accessElementInArray(array, index){
-  return array[index];
-}
-
-function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element);
-  return array;
-}
-
-function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element);
-  return array;
-}
-
-function destructivelyRemoveElementFromBeginningOfArray(array){
-  array.shift()
-  array
-}
-
-function destructivelyRemoveElementFromEndOfArray(array){
-  array.pop()
-  return array
-}
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
